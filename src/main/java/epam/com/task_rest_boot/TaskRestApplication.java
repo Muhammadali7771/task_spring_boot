9f5137a3026c51484117267c6c0b0d4f09f51af8
Muhammadali <akbarovmuhammadali777@gmail.com>
2025-04-05 00:54:00 +0500

    Implemented custom metrics for TraineeController

--- src/main/java/epam/com/task_rest_boot/TaskRestApplication.java
+++ src/main/java/epam/com/task_rest_boot/TaskRestApplication.java
@@ -2,6 +2,7 @@ package epam.com.task_rest_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 public class TaskRestApplication {
---
+++ src/main/java/epam/com/task_rest_boot/config/Config.java
@@ -0,0 +1,23 @@
+package epam.com.task_rest_boot.config;
+
+import io.micrometer.core.aop.CountedAspect;
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class Config {
+
+    @Bean
+    public CountedAspect countedAspect(MeterRegistry meterRegistry){
+        return new CountedAspect(meterRegistry);
+    }
+
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry meterRegistry){
+        return new TimedAspect(meterRegistry);
+    }
+}
